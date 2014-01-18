6c7dc11ee78673532b9e525d4a26e06dc1fb1eb8
dave@kapoq.com <dave@kapoq.com>
2014-01-17 18:14:38 -0600

    Remove obsolete Rspec config setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ require_relative '../lib/rack/http_spy'
 
 RSpec.configure do |config|
   config.order = "random"
-  config.raise_errors_for_deprecations!
   config.include Rack::Test::Methods
   config.after(:each) do
     WebMock::CallbackRegistry.reset
