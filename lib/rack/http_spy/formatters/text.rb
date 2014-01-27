1f500f150c0ed175dbfeacde00529ecbf31c8751
dave@kapoq.com <dave@kapoq.com>
2014-01-27 09:54:33 -0600

    Refactor Report::Text

--- lib/rack/http_spy/formatters/text.rb
+++ lib/rack/http_spy/formatters/text.rb
@@ -7,23 +7,37 @@ module Rack
         " Total: "
       end
 
+      def header
+        @header ||= [title, '=' * title.chars.to_a.length]
+      end
+
+      def body
+        @body ||= generate_body
+      end
+
+      def footer
+        @footer ||= begin
+                      lpad = [body.max.length - total_label.length - 1, 0].max
+                      ["#{' ' * lpad}#{total_label}#{total}", ""]
+                    end
+      end
+
       def to_s
-        Array.new.tap do |r|
-          r << title
-          r << ('=' * title.chars.to_a.length)
-          list.each do |hsh, req|
-            line = "#{req[:sig].method.upcase} #{req[:sig].uri.to_s}: #{req[:count]}"
-            line = ANSI::Code.red { line } if req[:count] > 1
-            r << line
-          end
-          lpad = r.max.length - total_label.length - 1
-          r << "#{' ' * lpad}#{total_label}#{total}"
-          r << ''
-        end.join($/)
+        [header, body, footer].flatten.join($/)
       end
 
       def content_type
         "text/plain"
+      end
+
+      private
+
+      def generate_body
+        list.each_with_object([]) do |(hsh, req), a|
+          line = "#{req[:sig].method.upcase} #{req[:sig].uri.to_s}: #{req[:count]}"
+          line = ANSI::Code.red { line } if req[:count] > 1
+          a << line
+        end
       end
     end
   end
